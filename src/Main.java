d12745c3a3d51b6c89ecd2c908752dad8876d989
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-03-18 18:49:57 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]){
+        String orgstr = "Java - двигатель Интернета";
+        String substr = orgstr.substring(7,26);
+        System.out.println("orgstr: "+ orgstr);
+        System.out.println("substr: "+ substr);
+    }
+}
